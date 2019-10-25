f091e5fdc039d987c1797792a290ec11fd72633d
san.ferreiro <santiago.ferreiro@accenture.com>
2019-10-24 21:19:25 -0300

    fixed async

--- app/routes/mongo_routes.js
+++ app/routes/mongo_routes.js
@@ -6,34 +6,40 @@ const fetch = require('node-fetch')
 
 var headers;
 var mongoResponse={
-    data:"",
-    errors
+    data:[],
+    errors:{
+        array: [],
+        quantity:0
+    }
 };
-var errors = {
-    array:"",
-    quantity:Number(0)
-}
+
+    
+
 
 
 module.exports = function(app){
 
-    app.get('/folder', (req, res)=>{
-        headers = req.headers
+    app.get('/:user/folder', (req, res)=>{
+        user = req.headers.user
+        console.log("user: "+user)
         
         fetch(mongoConst.URL+mongoConst.ENDPOINT_GET_ALL,{method:HTTP_METHODS.GET, headers:headers})
         .then(jsonUtil)
         .then(data =>{
-            mongoResponse.data = data;
+            mongoResponse.data[0] = data;
         })
         .catch(err =>{
             mongoResponse.errors.array[mongoResponse.errors.quantity] = err;
             mongoResponse.errors.quantity++;
         })
-
-        if (mongoResponse.errors.quantity != 0) {
-            res.send(mongoResponse.errors.array)
-        }else{
-            res.send(mongoResponse.data)
-        }
+        .finally(response => {
+            if (mongoResponse.errors.quantity != 0) {
+                console.log("Errores: "+mongoResponse.errors.array)
+                res.send(mongoResponse.errors.array)
+            }else{
+                console.log("Data: "+mongoResponse.data)
+                res.send(mongoResponse.data)
+            }
+        })  
     })
 }
